605f593486ccbeeb1868f9d8d4548497ca1c84ec
Your Name <sergio.gutierrez@estudiantesunibague.edu.com>
2026-08-14 13:24:20 -0500

    Rango

---
+++ usuarios.py
@@ -0,0 +1,7 @@
+cuantos = int(input("Cuantosw usuaarios va a crear?"))
+
+usuarios = []
+
+for i in range(cuantos):
+    nombre = input("inserte el nombre del usuario: ")
+    usuarios.append(nombre)
